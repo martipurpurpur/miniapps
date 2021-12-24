ca507cbe3fe3de929cb868f5a79f9951a85f3d8d
martipurpurpur <m.melnikova@unoanimo.ru>
2021-12-25 00:33:24 +0300

    added apps files, not found page, changed settings

--- miniapp/urls.py
+++ miniapp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:pk>/', views.miniapp)
+    path('<int:pk>/', views.miniapp),
+    path('about/', views.about)
 ]
--- miniapp/views.py
+++ miniapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import miniapp_db
+from services import *
 
 
 def miniapp(request, pk):
@@ -9,4 +10,13 @@ def miniapp(request, pk):
         'name': name,
         'description': description,
     }
-    return render(request, 'homepage/app.html', context)
+    if name == "Рандомайзер":
+        return render(request, 'homepage/apps/app_random.html', context)
+    elif name == "Мировое время":
+        return render(request, 'homepage/apps/app_time.html', context)
+    else:
+        return render(request, 'homepage/404.html')
+
+
+def about(request):
+    return render(request, 'homepage/about.html')

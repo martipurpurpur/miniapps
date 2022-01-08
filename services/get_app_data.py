a682f330fa8160cf4a430e81e9b58af592794717
martipurpurpur <m.melnikova@unoanimo.ru>
2022-01-08 14:35:11 +0300

    added getting worldtime by city name

--- services/get_app_data.py
+++ services/get_app_data.py
@@ -1,8 +1,11 @@
-
-from miniapps.models import MiniApp
-from random import randint
 import codecs
 import requests
+from random import randint
+from geopy.geocoders import Nominatim
+from timezonefinder import TimezoneFinder
+from pytz import timezone, utc
+from datetime import datetime
+
 
 def get_app_data(params, slug):
     if slug == 'randomizer':
@@ -19,10 +22,15 @@ def get_app_data(params, slug):
         result = None
     return result
 
+
 def worldtime(params):
     city = params['city']
-    result = city
+    geolocator = Nominatim(user_agent="multiapp")
+    location = geolocator.geocode(city, language="en")
+    points = {"lat": location.latitude, "lng": location.longitude}
+    result = get_time(**points)
     return result
+
 
 def randomizer(params):
     min = int(params['min_rand'])
@@ -30,14 +38,17 @@ def randomizer(params):
     result = randint(min, max)
     return result
 
+
 def telegrambot(params):
     result = params
     return result
+
 
 def encoder(params):
     text = params['rot']
     result = codecs.encode(text, 'rot13')
     return result
+
 
 def weather(params):
     city = params['city']
@@ -48,3 +59,13 @@ def weather(params):
     }
     response = requests.get(url, params=weather_parameters)
     return response.text
+
+
+def get_time(*, lat, lng):
+    """
+    Getting formatted time by longitude and latitude
+    """
+    tf = TimezoneFinder()
+    target_time_zone = timezone(tf.timezone_at(lat=lat, lng=lng))
+    result_time = datetime.now(tz=target_time_zone)
+    return result_time.strftime('%H:%M:%S %Z%z')
